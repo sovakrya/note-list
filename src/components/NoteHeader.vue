b4937340180beb3e52abcb85c8fa34be7629132f
sovaKrya <tyan_34ee@mail.ru>
2024-10-31 15:56:49 +0300

    added HomePage and NoteHeader

--- src/components/NoteHeader.vue
+++ src/components/NoteHeader.vue
@@ -6,21 +6,46 @@ const noteText = ref('')
 
 <template>
   <div class="header-box">
-    <input type="text" v-model="noteText" class="header-input" />
-    <button>+</button>
+    <input
+      type="text"
+      v-model="noteText"
+      class="header-input"
+      placeholder="Добавьте новую заметку..."
+    />
+    <button class="header-add-btn">+</button>
   </div>
 </template>
 
 <style scoped>
 .header-box {
   display: flex;
-  flex-direction: column;
   align-items: center;
-  width: 400px;
-  border: solid 1px;
+  width: 600px;
+  height: 100px;
+  gap: 8px;
 }
 
 .header-input {
   flex-grow: 1;
+  height: 30px;
+  padding: 4px;
+  border: solid 1px rgb(146, 146, 146);
+  border-radius: 5px;
+}
+
+.header-add-btn {
+  height: 30px;
+  width: 30px;
+  font-size: 26px;
+  cursor: pointer;
+  background-color: rgb(63, 88, 201);
+  border: none;
+  color: aliceblue;
+  border-radius: 5px;
+  transition: all 1s;
+}
+
+.header-add-btn:hover {
+  background-color: rgb(110, 131, 223);
 }
 </style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import NoteHeader from '@/components/NoteHeader.vue'
+</script>
+
+<template>
+  <div class="main-box">
+    <NoteHeader />
+  </div>
+</template>
+
+<style scoped>
+.main-box {
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  align-items: center;
+}
+</style>
