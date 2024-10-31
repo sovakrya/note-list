e037d1cbe3bf7532e7f0dea09d174a5bd863a66b
sovaKrya <tyan_34ee@mail.ru>
2024-10-31 15:41:01 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import NoteHeader from './components/NoteHeader.vue'
+</script>
+
+<template>
+  <main class="main-box">
+    <NoteHeader />
+  </main>
+</template>
+
+<style scoped>
+.main-box {
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+}
+</style>
---
+++ src/components/NoteHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const noteText = ref('')
+</script>
+
+<template>
+  <div class="header-box">
+    <input type="text" v-model="noteText" class="header-input" />
+    <button>+</button>
+  </div>
+</template>
+
+<style scoped>
+.header-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  border: solid 1px;
+}
+
+.header-input {
+  flex-grow: 1;
+}
+</style>
